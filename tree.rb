ec93378025b1b7d475619f218dfd31b439b757e5
wallebach <114591465+wallebach@users.noreply.github.com>
2022-10-18 17:36:33 +0200

    add 'delete', 'leftmost' and 'find' methods

--- tree.rb
+++ tree.rb
@@ -40,13 +40,54 @@ class Tree
 
         root
     end
+
+    def delete(value)
+        @root = deleteRecursive(@root, value)
+    end
+
+    def deleteRecursive(root, value)
+        if root.value == value
+            root = nil 
+            return root
+        end
+
+        if value < root.value
+            root.left = deleteRecursive(root.left, value)
+        elsif value > root.value
+            root.right = deleteRecursive(root.right, value)
+        else
+            return root.right if root.left.nil?
+            return root.left if root.right.nil?
+
+            leftmost_leaf = leftmost_leaf(root.right)
+            root.value = leftmost_leaf.value
+            root.right = deleteRecursive(leftmost_leaf, root.right.value)
+        end
+        root
+    end
+
+    def leftmost_leaf(root)
+        root = root.left until root.left.nil?
+        root
+    end
+
+    def find(value)
+        node = findRecursive(@root, value)
+    end
+
+    def findRecursive(root, value)
+        return root if root.nil? || root.value == value
+
+        value < root.value ? findRecursive(root.left, value) : findRecursive(root.right, value)
+    end
+
 end
 
 def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
-  end
+end
 
 data = [1,2,3,4,5,6,7,15,16]
 
@@ -56,4 +97,10 @@ tree.insert(12)
 tree.insert(8)
 tree.insert(9)
 
+# tree.delete(4)
+# tree.delete(7)
+
 pretty_print(tree.root)
+
+value_to_find = tree.find(12)&.value
+puts "Find element 8: #{value_to_find}"
